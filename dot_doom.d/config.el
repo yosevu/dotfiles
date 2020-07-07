ee029fdf395e2aa79dc9d184bcb0dfc2a751c4fd
Yosevu Kilonzo <yosevu@heysparkbox.com>
2020-07-07 01:22:08 -0400

    (emacs): set up pomidor

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -365,3 +365,15 @@
   (deft-use-filter-string-for-filename t)
   (deft-default-extension "org")
   (deft-directory +org-roam-path))
+
+(use-package pomidor
+  :bind (("<f12>" . pomidor))
+  :config (setq pomidor-sound-tick nil
+                pomidor-sound-tack nil)
+  :hook (pomidor-mode . (lambda ()
+                          (display-line-numbers-mode -1) ; Emacs 26.1+
+                          (setq left-fringe-width 0 right-fringe-width 0)
+                          (setq left-margin-width 2 right-margin-width 0)
+                          ;; force fringe update
+                          (set-window-buffer nil (current-buffer)))))
+
