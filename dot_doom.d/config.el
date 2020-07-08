67a5e1208226ddcd9e1b441588006682688aa796
Yosevu Kilonzo <yosevu@heysparkbox.com>
2020-07-08 08:53:28 -0400

    (emacs): clean up unused config

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -110,7 +110,6 @@
  web-mode-markup-indent-offset 2
  web-mode-code-indent-offset 2
  web-mode-css-indent-offset 2
- ;; mac-command-modifier 'meta
  js-indent-level 2
  json-reformat:indent-width 2
  prettier-js-args '("--single-quote")
@@ -184,6 +183,37 @@
          ;; After the last group, the agenda will display items that didn't
          ;; match any of these groups, with the default order position of 99
          ))
+
+;; (let ((org-super-agenda-groups
+;;        '(;; Each group has an implicit boolean OR operator between its selectors.
+;;          (:name "Today"  ; Optionally specify section name
+;;                 :time-grid t  ; Items that appear on the time grid
+;;                 :todo "TODAY")  ; Items that have this TODO keyword
+;;          (:name "Important"
+;;                 ;; Single arguments given alone
+;;                 :tag "bills"
+;;                 :priority "A")
+;;          (:name "Habit"
+;;           :habit t
+;;           :scheduled t)
+;;          ;; Groups supply their own section names when none are given
+;;          (:todo "WAITING" :order 8)  ; Set order of this section
+;;          (:todo ("SOMEDAY" "TO-READ" "CHECK" "TO-WATCH" "WATCHING")
+;;                 ;; Show this group at the end of the agenda (since it has the
+;;                 ;; highest number). If you specified this group last, items
+;;                 ;; with these todo keywords that e.g. have priority A would be
+;;                 ;; displayed in that group instead, because items are grouped
+;;                 ;; out in the order the groups are listed.
+;;                 :order 9)
+;;          (:priority<= "B"
+;;                       ;; Show this section after "Today" and "Important", because
+;;                       ;; their order is unspecified, defaulting to 0. Sections
+;;                       ;; are displayed lowest-number-first.
+;;                       :order 1)
+;;          ;; After the last group, the agenda will display items that didn't
+;;          ;; match any of these groups, with the default order position of 99
+;;          )))
+;;   (org-agenda nil "a"))
 
 (require 'htmlize)
 (require 'org-roam)
@@ -271,39 +301,6 @@
           (insert (concat "\n* Backlinks\n") links))))))
 
   (add-hook 'org-export-before-processing-hook 'my/org-export-preprocessor)
-
-;; org-roam org-export hook to add backlinks with content
-;; (defun my/org-roam--backlinks-list-with-content (file)
-;;   (with-temp-buffer
-;;     (if-let* ((backlinks (org-roam--get-backlinks file))
-;;               (grouped-backlinks (--group-by (nth 0 it) backlinks)))
-;;         (progn
-;;           (insert (format "\n\n* %d Backlinks\n"
-;;                           (length backlinks)))
-;;           (dolist (group grouped-backlinks)
-;;             (let ((file-from (car group))
-;;                   (bls (cdr group)))
-;;               (insert (format "** [[file:%s][%s]]\n"
-;;                               file-from
-;;                               (org-roam--get-title-or-slug file-from)))
-;;               (dolist (backlink bls)
-;;                 (pcase-let ((`(,file-from _ ,props) backlink))
-;;                   (insert (s-trim (s-replace "\n" " " (plist-get props :content))))
-;;                   (insert "\n\n")))))))
-;;     (buffer-string)))
-
-;;   (defun my/org-export-preprocessor (backend)
-;;     (let ((links (my/org-roam--backlinks-list-with-content (buffer-file-name))))
-;;       (unless (string= links "")
-;;         (save-excursion
-;;           (goto-char (point-max))
-;;           (insert (concat "\n* Backlinks\n") links)))))
-
-;;   (add-hook 'org-export-before-processing-hook 'my/org-export-preprocessor)
-
-;; Publish org files and assets to public HTML
-
-;; (setq org/head-extra "<link rel='stylesheet' type='text/css' href='css/main.css'/>")
 
 ;; org-publish config
 (require 'ox-publish)
