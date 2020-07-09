42136556e0047f6f8e5516534e9266db3ae0d19f
Yosevu Kilonzo <yosevu@heysparkbox.com>
2020-07-08 23:17:40 -0400

    (emacs): clean up unused config

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -168,37 +168,6 @@
           :deadline past
           :order 6)))
 
-;; (let ((org-super-agenda-groups
-;;        '(;; Each group has an implicit boolean OR operator between its selectors.
-;;          (:name "Today"  ; Optionally specify section name
-;;                 :time-grid t  ; Items that appear on the time grid
-;;                 :todo "TODAY")  ; Items that have this TODO keyword
-;;          (:name "Important"
-;;                 ;; Single arguments given alone
-;;                 :tag "bills"
-;;                 :priority "A")
-;;          (:name "Habit"
-;;           :habit t
-;;           :scheduled t)
-;;          ;; Groups supply their own section names when none are given
-;;          (:todo "WAITING" :order 8)  ; Set order of this section
-;;          (:todo ("SOMEDAY" "TO-READ" "CHECK" "TO-WATCH" "WATCHING")
-;;                 ;; Show this group at the end of the agenda (since it has the
-;;                 ;; highest number). If you specified this group last, items
-;;                 ;; with these todo keywords that e.g. have priority A would be
-;;                 ;; displayed in that group instead, because items are grouped
-;;                 ;; out in the order the groups are listed.
-;;                 :order 9)
-;;          (:priority<= "B"
-;;                       ;; Show this section after "Today" and "Important", because
-;;                       ;; their order is unspecified, defaulting to 0. Sections
-;;                       ;; are displayed lowest-number-first.
-;;                       :order 1)
-;;          ;; After the last group, the agenda will display items that didn't
-;;          ;; match any of these groups, with the default order position of 99
-;;          )))
-;;   (org-agenda nil "a"))
-
 (require 'htmlize)
 (require 'org-roam)
 
@@ -358,5 +327,3 @@
                           ;; force fringe update
                           (set-window-buffer nil (current-buffer)))))
 
-(require 'company-terraform)
-(company-terraform-init)
