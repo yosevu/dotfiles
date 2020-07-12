43b8f8acf084d03d7141f740adfb90d32f00c8bc
Yosevu Kilonzo <yosevu@heysparkbox.com>
2020-07-12 14:56:30 -0400

    (emacs): configure pomidor sounds

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -329,5 +329,11 @@
                           (setq left-fringe-width 0 right-fringe-width 0)
                           (setq left-margin-width 2 right-margin-width 0)
                           ;; force fringe update
-                          (set-window-buffer nil (current-buffer)))))
+                          (set-window-buffer nil (current-buffer))
+                          (setq pomidor-play-sound-file
+                                (lambda (file)
+                                  (start-process "my-pomidor-play-sound"
+                                                 nil
+                                                 "mplayer"
+                                                 file))))))
 
