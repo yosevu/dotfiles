437c44eef2045b5c22c66e91a4ca97e3276921b0
Yosevu Kilonzo <yosevuk@gmail.com>
2020-10-29 07:33:45 -0400

    (zsh): update aliases

--- dot_oh-my-zsh/custom/aliases.zsh
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -1,5 +1,8 @@
 alias dot='chezmoi'
 
-alias o='cd ~/Dropbox/org/'
+alias drive='cd ~/Google\ Drive/'
+alias box='cd ~/Dropbox/'
+alias org='cd ~/Dropbox/org/'
+alias roam='cd ~/Dropbox/org/roam'
 alias p='cd ~/Documents/projects/personal/'
 alias w='cd ~/Documents/projects/work/'
