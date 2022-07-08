6f1b2fb33ca08497066513efd22196e96a8952c2
Yosevu Kilonzo <yosevuk@gmail.com>
2022-07-08 15:02:33 +0900

    chore: update .emacs-profiles.el

--- dot_emacs-profiles.el
+++ dot_emacs-profiles.el
@@ -1,3 +1,3 @@
-(("default" . ((user-emacs-directory . "~/.emacs/doom")
+(("default" . ((user-emacs-directory . "~/.emacs/doom-emacs")
                (env . (("DOOMDIR" . "~/.doom.d")))))
  ("rational" . ((user-emacs-directory . "~/.emacs/rational"))))
